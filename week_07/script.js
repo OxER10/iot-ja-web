4ee6a758034f013f454fcfd270978f28c2b04ac8
Oskari Järvinen <oskari.jarvinen@tuni.fi>
2025-02-16 11:54:40 +0200

    Create exercise folders

---
+++ week_07/script.js
@@ -0,0 +1,78 @@
+// console.log('testi 1');
+// console.log('testi 2');
+// console.log('testi 3');
+
+// var message = "höpönlöpön";
+// let month = "February";
+// const workday = 5;
+
+// const objekti = {key1: "value", key2: "value2" };
+// // const CONSTANT_VARIABLE = value;
+
+
+// objekti.key1 = "testi";
+// console.log(objekti);
+
+// let message = "hello";
+// console.log(message);
+
+
+// function test (x, y) {
+//     console.log("Olen funktio :)")
+//     var x = 10;
+// }
+
+// if (true) {
+//     let x = 10;
+//     console.log(x); // Output: 10;
+// }
+// console.log(x); // Error: x is not defined
+
+
+// let x = "stringi";
+// let y = 6;
+// let z = false;
+// console.log(typeof(x));
+// console.log(typeof(y));
+// console.log(typeof(z));
+
+
+// const name = "John Doe";
+// const age = 30;
+
+// const message = `Hello, my name is ${name} and I am ${age} years old.
+// rivi 2
+// rivi 3
+// `;
+
+
+// let age = 18;
+// let message = age >= 16 ? 'You can drive.' : 'You cannot drive.';
+// console.log(message);
+
+
+let day;
+switch (new Date().getDay()) {
+    case 0:
+        day = 'Sunday';
+        break;
+    case 1:
+        day = 'Monday';
+        break;
+    case 2:
+        day = 'Tuesday';
+        break;
+    case 3:
+        day = 'Wednesday';
+        break;
+    case 4:
+        day = 'Thursday';
+        break;
+    case 5:
+        day = 'Friday';
+        break;
+    case 6:
+        day = 'Saturday';
+        break;
+}
+console.log(day);
